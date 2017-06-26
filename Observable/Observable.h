6baa487b94bfa21e8f2e1f58034e87b43c0d9fc6
devtak <hyotak.yun@gmail.com>
2017-06-26 23:52:26 +0900

    observable.framework

---
+++ Observable/Observable.h
@@ -0,0 +1,19 @@
+//
+//  Observable.h
+//  Observable
+//
+//  Created by devtak on 2017. 6. 26..
+//  Copyright (c) 2017 devtak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Observable.
+FOUNDATION_EXPORT double ObservableVersionNumber;
+
+//! Project version string for Observable.
+FOUNDATION_EXPORT const unsigned char ObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observable/PublicHeader.h>
+
+
